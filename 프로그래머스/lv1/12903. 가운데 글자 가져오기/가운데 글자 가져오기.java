f203496c5606efd74c7d8101129337647e7f0c52
LEE SangDo <108053426+SANGDOLEE@users.noreply.github.com>
2022-10-16 17:20:54 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 10.18 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s) {
+       String answer="";
+       int n=s.length();
+       if(n%2==1)
+           answer+=s.charAt(n/2);
+       else {
+           answer += s.charAt(n/2-1);
+           answer += s.charAt(n/2);
+       }
+       return answer;
+    }
+}
