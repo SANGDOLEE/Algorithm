cdbcaa014c3c994380ded7eed9123b06932e98da
L-SangDo <108053426+SANGDOLEE@users.noreply.github.com>
2022-10-19 16:40:32 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.03 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int max=numbers[0]*numbers[1];
+        if(numbers.length==2)
+            return max;
+        for(int i=0; i<numbers.length-1; i++){
+            for(int j=i+1; j<numbers.length; j++){
+               if(numbers[i]*numbers[j]>max){
+                    max=numbers[i]*numbers[j];
+                }
+            }
+        }
+        answer=max;
+        return answer;
+    }
+}
