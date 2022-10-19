fd899ae757057abe4fef72b61147b05cb7441ea9
L-SangDo <108053426+SANGDOLEE@users.noreply.github.com>
2022-10-19 16:00:55 +0900

    [level 1] Title: 예산, Time: 0.37 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.java
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 class Solution {
     public int solution(int[] d, int budget) {
        int answer=0;
-       int cnt=0;
        Arrays.sort(d); // 1 2 3 4 5
         for(int i=0; i<d.length; i++) {
             if (budget - d[i] >= 0) {
                 budget=budget-d[i];
-                cnt++;
+                answer++;
             }
         }
-      answer=cnt;
       return answer;
     }
 }
