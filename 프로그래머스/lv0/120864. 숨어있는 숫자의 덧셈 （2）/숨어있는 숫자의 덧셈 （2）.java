feeeff6207ce920a4f56913b06012e1717da6926
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2022-10-25 19:51:08 +0900

    Update 숨어있는 숫자의 덧셈 （2）.java

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -15,3 +15,22 @@ class Solution {
         return answer;
     }
 }
+
+
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+
+        String temp = "";
+        for(int i=0; i<my_string.length(); i++) {
+            if(Character.isDigit(my_string.charAt(i))) {
+                temp += my_string.charAt(i);
+            }
+            else {
+                if(temp.length() != 0) answer += Integer.parseInt(temp);
+                temp = "";
+            }
+        }
+        if(temp.length() != 0) answer += Integer.parseInt(temp);
+        return answer;
+    }
