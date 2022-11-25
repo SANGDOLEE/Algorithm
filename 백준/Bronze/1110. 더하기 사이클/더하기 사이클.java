dd3bd99d4bea90692fed2641d965f07dc4da4386
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2022-11-25 16:32:24 +0900

    [Bronze I] Title: 더하기 사이클, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws Exception {
+        Scanner kb=new Scanner(System.in);
+
+       int n=kb.nextInt();
+
+       int cnt=0;
+       int copy=n;
+
+       while(true){
+           n=((n%10)*10)+(((n/10)+(n%10))%10);
+           cnt++;
+           if(copy==n){
+               break;
+           }
+       }
+        System.out.println(cnt);
+    }
+}
+
