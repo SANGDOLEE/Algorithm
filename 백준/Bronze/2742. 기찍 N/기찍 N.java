23732c6c0bc75afaeb2df01a37a8aa9eb05ca638
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2022-11-27 15:05:13 +0900

    [Bronze IV] Title: 기찍 N, Time: 1144 ms, Memory: 34248 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws Exception {
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        for(int i=n; i>=1; i--) System.out.println(i);
+    }
+}
+
