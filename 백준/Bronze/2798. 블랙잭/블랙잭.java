33867e16f334bdf005baa372340d6ce302e5bc86
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2022-11-28 17:01:04 +0900

    [Bronze II] Title: 블랙잭, Time: 228 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws Exception {
+        Scanner kb = new Scanner(System.in);
+
+        int n=kb.nextInt();
+        int m=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=kb.nextInt();
+        }
+
+        int min=0;
+        int sum=0;
+        for(int i=0; i<n-2; i++){
+            for(int j=i+1; j<n-1; j++){
+                for(int k=j+1; k<n; k++){
+                    sum=arr[i]+arr[j]+arr[k];
+                    if(min<sum&&sum<=m){
+                        min=sum;
+                    }
+                }
+            }
+        }
+        System.out.println(min);
+    }
+}
+
