9a87b41f88e06c272505df1c4449732e5d7b5a4b
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2022-11-30 15:43:39 +0900

    [Bronze I] Title: 이항 계수 1, Time: 216 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws Exception {
+        Scanner kb=new Scanner(System.in);
+        int n=kb.nextInt();
+        int k=kb.nextInt();
+
+        int a=1;
+        int b=1;
+        for(int i=n; i>n-k; i--){
+            a*=i;
+        }
+        for(int i=k; i>=1; i--){
+            b*=i;
+        }
+        System.out.println(a/b);
+    }
+}
+
