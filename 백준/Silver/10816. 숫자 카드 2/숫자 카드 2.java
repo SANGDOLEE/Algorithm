b0b4c896db41dedfcd6a45ed1e5b47a1048cb68b
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2022-12-06 14:36:06 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 2528 ms, Memory: 357724 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -6,20 +6,17 @@ class Main {
          HashMap<Integer,Integer> map=new HashMap<>();
 
          int a=kb.nextInt();
-         int[] arr2=new int[a];
          for(int i=0; i<a; i++){
-             arr2[i]=kb.nextInt();
-             map.put(arr2[i], map.getOrDefault(arr2[i],0)+1);
+             int n=kb.nextInt();
+             map.put(n, map.getOrDefault(n,0)+1);
          }
 
          int b=kb.nextInt();
-         int[] arr=new int[b];
-         for(int i=0; i<b; i++) arr[i]=kb.nextInt();
-
          StringBuilder sb=new StringBuilder();
          for(int i=0; i<b; i++){
-             if(map.containsKey(arr[i])){
-                 sb.append(map.get(arr[i])).append(" ");
+             int k=kb.nextInt();
+             if(map.containsKey(k)){
+                 sb.append(map.get(k)).append(" ");
              }else{
                  sb.append(0).append(" ");
              }
