b93029d73607ab4d846a025b153133d30b619afe
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2022-12-09 16:43:09 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 646.37 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -1,11 +1,13 @@
 class Solution {
-       public int combi(int n, int r){
+        int[][] dy=new int[31][31];
+    public int DFS(int n, int r){
+        if(dy[n][r]>0) return dy[n][r];
         if(r==0||n==r){
             return 1;
         }else
-            return combi(n-1,r-1)+combi(n-1,r);
+            return DFS(n-1,r-1)+DFS(n-1,r);
     }
     public int solution(int balls, int share) {
-        return combi(balls, share);
+        return DFS(balls, share);
     }
 }
