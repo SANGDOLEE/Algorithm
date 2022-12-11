48a7ce87424f34f913b150a9d6678a4ea93a5024
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2022-12-11 14:11:10 +0900

    [level 0] Title: 종이 자르기, Time: 0.01 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int M, int N) {
+          int answer = 0;
+        int num1=M-1;
+        int num2=((N-1)*M);
+        answer=num1+num2;
+        return answer;
+    }
+}
