eda63ab406858a567d14b74935d0618f331ff9b1
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2022-12-24 18:08:22 +0900

    [level 0] Title: 안전지대, Time: 0.63 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.java
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -1,32 +1,32 @@
 class Solution {
-     private static void destroyArea(int i, int j, int n, int[][] copy){
-        for(int x=i-1; x<=i+1; x++){
-            if(x<0||x>=n) continue;
-            else{
-                for(int y=j-1; y<=j+1; y++){
-                    if(y<0||y>=n) continue;
-                    else copy[x][y]=1;
-                }
+    public int solution(int[][] board) {
+          int n=board.length;
+        int[][] area=new int[n+2][n+2];
+
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<n+1; j++){
+                area[i][j]=board[i-1][j-1];
             }
         }
-    }
-    public int solution(int[][] board) {
-        int n=board.length;
-        int[][] area=new int[n][n];
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(board[i][j]==1){
-                    destroyArea(i,j,n,area);
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(area[i][j]==1){
+                    for(int x=i-1; x<=i+1; x++){
+                        for(int y=j-1; y<=j+1; y++){
+                            if(area[x][y]!=1) area[x][y]=2;
+                        }
+                    }
                 }
             }
         }
         int answer=0;
-        for(int i=0; i<area.length; i++){
-            for(int j=0; j<area.length; j++){
-                if(area[i][j]==0)
-                    answer++;
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(area[i][j]==0) answer++;
+                System.out.print(area[i][j]);
             }
+            System.out.println();
         }
         return answer;
     }
