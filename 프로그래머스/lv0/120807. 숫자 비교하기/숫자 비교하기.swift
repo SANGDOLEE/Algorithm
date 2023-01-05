56c90da8808fcebf79ab9866c8e904db0e8bb6bf
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2023-01-05 23:25:21 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.swift
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.swift
@@ -3,9 +3,9 @@ import Foundation
 func solution(_ num1:Int, _ num2:Int) -> Int {
     var answer=0
     if num1==num2{
-        answer+=1
+        answer = 1
     } else{
-        answer-=1
+        answer = -1
     }
     return answer
 }
