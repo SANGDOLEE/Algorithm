7b13128bd03315d55f57230a05d8c6262b4de4fa
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2023-03-06 18:01:39 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.35 ms, Memory: 65.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+
+        PriorityQueue<Integer> Q = new PriorityQueue<>();
+
+        for(int i=0; i<score.length; i++){
+            Q.add(score[i]);
+            if(Q.size()>k){
+                Q.poll();
+            }
+            answer[i]=Q.peek();
+        }
+
+        return answer;
+    }
+}
