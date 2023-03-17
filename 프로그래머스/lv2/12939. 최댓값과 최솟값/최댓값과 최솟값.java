6ef33c0ccc6e7ebdff8650d0ea9c5e8b6eb509b4
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2023-03-17 15:27:33 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 12.57 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+
+        String[] str=s.split(" ");
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<str.length; i++){
+            list.add(Integer.valueOf(str[i]));
+        }
+        Collections.sort(list);
+        int min = list.get(0);
+        int max = list.get(list.size()-1);
+        answer=min+" "+max;
+        return answer;
+    }
+}
