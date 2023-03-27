664b81511f55e98b23fd7909908480ef0787b4fe
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2023-03-27 15:24:09 +0900

    [level 2] Title: 최솟값 만들기, Time: 5.82 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -6,16 +6,18 @@ class Solution
     {
          int answer = 0;
 
-        Arrays.sort(A);
-        Arrays.sort(B);
-
-        int j = B.length-1;
-
-        for(int i=0; i<A.length; i++){
-            answer += A[i]*B[j];
-            j--;
+        PriorityQueue<Integer> a = new PriorityQueue<>();
+        PriorityQueue<Integer> b = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i = 0; i< A.length; i++){
+            a.add(A[i]);
+            b.add(B[i]);
         }
-
+        
+        while(!a.isEmpty()){
+            answer += a.poll() * b.poll();
+        }
+        
         return answer;
     }
 }
