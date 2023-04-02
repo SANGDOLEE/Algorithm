3afc6c2b8fd4af5e428e3d53e6767cdaaafacf33
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2023-04-02 15:08:37 +0900

    [level 2] Title: 숫자의 표현, Time: 0.73 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,12 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+           int answer = 0;
         int sum = 0;
         int lt = 0;
-        int m = n;
-        int[] arr = new int[m];
-        for(int i=0; i<m; i++) arr[i]=i+1;
-        for(int rt=0; rt<m; rt++){
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) arr[i]=i+1;
+        for(int rt=0; rt<n; rt++){
             sum+=arr[rt];
             if(sum==n) answer++;
             while(sum>=n){
