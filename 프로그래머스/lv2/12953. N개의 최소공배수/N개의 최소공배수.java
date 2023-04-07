38bb70697eab44ecda68062942649c0e3efaf08d
Lee Sang Do <108053426+SANGDOLEE@users.noreply.github.com>
2023-04-07 14:01:11 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,15 +1,15 @@
 class Solution {
     public int solution(int[] arr) {
         int answer = arr[0];
-        
+
         for(int i=0; i<arr.length; i++){
             answer = lcm(answer,arr[i]);
         }
-        
+
         return answer;
     }
     static int gcd(int a, int b){
-        if(a%b==0) return b;
+        if(b==0) return a;
         return gcd(b,a%b);
     }
     static int lcm(int a, int b){
