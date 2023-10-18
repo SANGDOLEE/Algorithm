f21e44591a7640a0a31416253a4564bbfd30e730
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-10-18 18:15:38 +0900

    [level unrated] Title: 배열 두배 만들기, Time: 0.12 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120809. 배열 두배 만들기/배열 두배 만들기.swift
+++ 프로그래머스/unrated/120809. 배열 두배 만들기/배열 두배 만들기.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-func solution(_ numbers: [Int]) -> [Int]{
-	return numbers.map(){$0 * 2}
+func solution(_ numbers:[Int]) -> [Int] {
+    
+    var result:[Int] = []
+    
+    for i in numbers {
+        result.append(i * 2)
+    }
+    return result
 }
