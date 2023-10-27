4cd850d869561322832201ae37e42a4141876d48
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-10-27 15:31:38 +0900

    [level unrated] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.swift
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.swift
@@ -4,13 +4,12 @@ func solution(_ my_string:String, _ n:Int) -> String {
     
     var result:String = ""
     
-    var count = 0
     for i in my_string {
-        count = 0
+        var count = 0
         while(count<n){
             result += String(i)
             count += 1
         }
     }
-    return result 
+    return result
 }
