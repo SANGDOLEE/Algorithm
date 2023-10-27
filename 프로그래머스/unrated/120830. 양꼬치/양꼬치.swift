df55ad6aca5480b010aa2ad8b4554e9091a2f666
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-10-27 14:26:09 +0900

    [level unrated] Title: 양꼬치, Time: 0.00 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120830. 양꼬치/양꼬치.swift
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.swift
@@ -1,6 +1,11 @@
 import Foundation
+
 func solution(_ n:Int, _ k:Int) -> Int {
     
-    return (n*12000)+(k*2000)-((n/10)*2000)
+    var nSum = n*12000
+    var kSum = k*2000
+    var service = n/10
+    var answer = nSum + kSum - (service*2000)
     
+    return answer
 }
