efc978188b8a1ca6bdb7727ddaa6cbeb436a9d6e
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-10-27 14:20:23 +0900

    [level unrated] Title: 배열의 평균값, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.swift
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.swift
@@ -1,10 +1,14 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> Double {
-    var sum = 0
-    for i in numbers{
-        sum += i
+    
+    var sum:Double = 0
+    
+    for i in numbers {
+        sum += Double(i)
     }
-    print(sum)
-    return Double(sum)/Double(numbers.count)
+    
+    var answer:Double = sum / Double(numbers.count)
+    return answer
+    
 }
