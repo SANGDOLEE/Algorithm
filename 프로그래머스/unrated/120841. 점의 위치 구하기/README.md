69857253b31917f3c6400d5a0c035227dce09078
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-10-27 14:38:56 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120841. 점의 위치 구하기/README.md
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 점의 위치 구하기 - 120841 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120841?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120841) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 0.01 ms
+메모리: 16.6 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 15:53:39
+2023년 10월 5일 14:38:54
 
 ### 문제 설명
 
--- 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.swift
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.swift
@@ -1,18 +1,19 @@
 import Foundation
 
 func solution(_ dot:[Int]) -> Int {
+    
     var a = dot[0]
     var b = dot[1]
     
     var answer = 0
-    if a>0 && b>0 {
+    if a > 0 && b > 0 {
         answer = 1
-    } else if a>0 && b<0 {
-        answer = 4
-    } else if a<0 && b>0 {
+    } else if a < 0 && b > 0 {
         answer = 2
-    } else {
+    } else if a < 0 && b < 0 {
         answer = 3
+    } else {
+        answer = 4
     }
     return answer
 }
