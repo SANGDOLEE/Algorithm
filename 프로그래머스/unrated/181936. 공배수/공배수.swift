26eb15f8b3723d39be073327302009e7adfea92e
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-11-01 15:30:24 +0900

    [level unrated] Title: 공배수, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.swift
+++ 프로그래머스/unrated/181936. 공배수/공배수.swift
@@ -2,9 +2,5 @@ import Foundation
 
 func solution(_ number:Int, _ n:Int, _ m:Int) -> Int {
     
-    if number%n == 0 && number%m == 0 {
-        return 1
-    }
-    
-    return 0
+    return (number%n == 0 && number%m == 0) ? 1 : 0
 }
