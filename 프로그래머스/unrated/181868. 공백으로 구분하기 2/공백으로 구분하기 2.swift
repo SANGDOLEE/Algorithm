1c59f171b8735547b7efef0cf9947dc0b7cf1528
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-11-02 15:26:10 +0900

    [level unrated] Title: 공백으로 구분하기 2, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.swift
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 func solution(_ my_string:String) -> [String] {
-    return my_string.components(separatedBy: .whitespaces).filter { !$0.isEmpty }
+    return my_string.split(separator: " ").map{ String($0)}
+    
 }
