2c48bc7e6219f42df4eff19261333431d4ca75c7
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-11-02 15:02:56 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/README.md
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 피자 나눠 먹기 (1) - 120814 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120814?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120814) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 0.01 ms
+메모리: 16.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 18:9:58
+2023년 11월 4일 15:2:50
 
 ### 문제 설명
 
--- 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.swift
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.swift
@@ -2,12 +2,15 @@ import Foundation
 
 func solution(_ n:Int) -> Int {
     var answer = 0
-    if n < 7 {
-        answer = 1
-    } else if n%7 == 0 {
-        answer = n/7
-    } else {
-        answer = n/7 + 1
+    
+    if n<8 {
+        answer=1
+    } else if n > 7 {
+        if n%7 == 0 {
+            answer = n/7
+        } else {
+            answer=(n/7)+1
+        }
     }
     return answer
 }
