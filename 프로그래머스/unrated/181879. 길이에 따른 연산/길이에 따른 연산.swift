c6cd60557c2b073405f24612b2d05b1e334d605c
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-11-03 12:49:31 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.swift
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.swift
@@ -2,14 +2,12 @@ import Foundation
 
 func solution(_ num_list:[Int]) -> Int {
     
-    var result = 1
+    var result = 0
     
     if num_list.count > 10 {
         result = num_list.reduce(0,+)
     } else {
-        for i in num_list {
-            result = result * i
-        }
+        result = num_list.reduce(1,*)
     }
     return result
 }
