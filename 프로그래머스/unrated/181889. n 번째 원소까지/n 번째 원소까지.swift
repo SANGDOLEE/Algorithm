c4989f52ee264903a7ce77caa427fd8e6cbb733c
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-11-03 13:38:36 +0900

    [level unrated] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.swift
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.swift
@@ -2,5 +2,14 @@ import Foundation
 
 func solution(_ num_list:[Int], _ n:Int) -> [Int] {
     
+    return Array(num_list.prefix(n))
+}
+/*
+import Foundation
+
+func solution(_ num_list:[Int], _ n:Int) -> [Int] {
+    
     return [Int](num_list.prefix(n))
 }
+
+*/
