7af444b969022ee5f11065ab39e89d98c228b6f9
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-11-14 13:46:06 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    
+    var result = 0
+    
+    if n%2 == 1
+    {
+        for i in stride(from: 1, through: n, by: 2) {
+           result += i
+        }
+    }
+    else
+    {
+        for i in stride(from: 0, through: n, by: 2){
+            result += i*i
+        }
+    }
+    return result
+}
