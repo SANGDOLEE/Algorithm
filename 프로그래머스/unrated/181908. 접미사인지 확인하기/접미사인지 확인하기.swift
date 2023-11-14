43b0c12af7582d1cbc124d08e372380c22dcc4bb
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-11-14 12:48:51 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.swift
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.swift
@@ -2,11 +2,6 @@ import Foundation
 
 func solution(_ my_string:String, _ is_suffix:String) -> Int {
     
-    var length: Int = is_suffix.count
     
-    var temp:String = String(my_string.suffix(length))
-
-    
-    
-    return (temp == is_suffix) ? 1 : 0
+    return my_string.suffix(is_suffix.count) == is_suffix ? 1 : 0
 }
