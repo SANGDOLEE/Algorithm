be0a56f7f456970b937784dbbc2487ba1b8370cf
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-11-14 14:04:57 +0900

    [level unrated] Title: 카운트 다운, Time: 0.09 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.swift
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.swift
@@ -3,8 +3,8 @@ import Foundation
 func solution(_ start:Int, _ end_num:Int) -> [Int] {
     var result = [Int]()
     
-    for i in end_num...start {
+    for i in (end_num...start).reversed() {
         result.append(i)
     }
-    return result.reversed()
+    return result
 }
