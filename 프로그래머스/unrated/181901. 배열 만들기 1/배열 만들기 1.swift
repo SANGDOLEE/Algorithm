15a46069790b2b06908c38352156b629d3cda710
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-11-15 16:08:20 +0900

    [level unrated] Title: 배열 만들기 1, Time: 0.07 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ n:Int, _ k:Int) -> [Int] {
+    
+    var result = [Int]()
+    
+    for i in stride(from: k, through: n, by: k){
+        result.append(i)
+    }
+    return result
+}
