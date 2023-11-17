8c1a8c72c6b91ff27476bd9d8033b53c317a22ad
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-11-17 15:58:30 +0900

    [level unrated] Title: 문자열 바꿔서 찾기, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.swift
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.swift
@@ -2,13 +2,5 @@ import Foundation
 
 func solution(_ myString:String, _ pat:String) -> Int {
     
-    var result = ""
-    for i in myString {
-        if i == "A" {
-            result += "B"
-        } else {
-            result += "A"
-        }
-    }
-    return result.contains(pat) ? 1 : 0
+    return myString.map{ $0 == "A" ? "B" : "A"}.joined().contains(pat) ? 1 : 0
 }
