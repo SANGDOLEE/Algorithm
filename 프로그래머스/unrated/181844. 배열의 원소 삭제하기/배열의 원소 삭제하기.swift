cdc0e286400c97578e62c3f8b71a2d0031442792
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-11-22 15:42:25 +0900

    [level unrated] Title: 배열의 원소 삭제하기, Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ arr:[Int], _ delete_list:[Int]) -> [Int] {
+    
+    var answer = [Int]()
+    
+    for i in arr {
+        if !delete_list.contains(i) {
+            answer.append(i)
+        } else {
+            continue
+        }
+    }
+    return answer
+}
