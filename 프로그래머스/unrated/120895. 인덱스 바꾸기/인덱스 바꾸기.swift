34c6e5f899243905e9a64074170567ea5be40e2a
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-11-24 14:43:23 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.07 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.swift
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.swift
@@ -2,15 +2,8 @@ import Foundation
 
 func solution(_ my_string:String, _ num1:Int, _ num2:Int) -> String {
     
-    var a = my_string.index(my_string.startIndex, offsetBy: num1)
-    var b = my_string.index(my_string.startIndex, offsetBy: num2)
+   var array = Array(my_string)
     
-    var answer = my_string
-    
-    let ch1 = my_string[a]
-    let ch2 = my_string[b]
-    
-    answer.replaceSubrange(a...a, with: String(ch2))
-    answer.replaceSubrange(b...b, with: String(ch1))
-    return answer
+    array.swapAt(num1, num2)
+    return array.map{ String($0) }.joined()
 }
