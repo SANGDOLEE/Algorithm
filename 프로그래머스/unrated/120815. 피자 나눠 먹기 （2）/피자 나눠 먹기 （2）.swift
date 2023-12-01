e13a80bd5e5756ca3c70d7c2e041e4217486b496
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-12-01 14:40:09 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    
+    return lcm(n,6)/6
+}
+func gcd(_ a:Int, _ b:Int) -> Int {
+    if b == 0{
+        return a
+    } else {
+        return gcd(b,a % b)
+    }
+}
+func lcm(_ a:Int, _ b:Int) -> Int {
+    return a*b / gcd(a,b)
+}
