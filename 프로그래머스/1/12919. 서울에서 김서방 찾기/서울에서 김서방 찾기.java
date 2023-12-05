6e565b44f9e3b12fad9de695a9b3e29ed26fd07f
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-12-05 15:36:08 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.51 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.Scanner;
+class Solution {
+    public String solution(String[] seoul) {
+
+
+        return "김서방은 "+Arrays.asList(seoul).indexOf("Kim")+"에 있다";
+    }
+}
