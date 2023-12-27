4a3926f5ea92939786721d5451b2df847c2e51bb
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2023-12-27 16:42:43 +0900

    [level 1] Title: 과일 장수, Time: 0.41 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,29 +1,18 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
-         int answer = 0;
+     int answer = 0;
 
         Arrays.sort(score);
-        int count = score.length / m ;
 
         int index = 0;
         ArrayList<Integer> list = new ArrayList<>();
-        int min = 0;
         for(int i=score.length-1; i>=0; i--) {
-            if (list.size() != m) {
-                list.add(score[i]);
-                min = score[i];
-                if( i==0 && list.size() == m){
-                    answer += min * m;
-                    break;
-                }
-            } else {
-                answer += min * m;
-                index++;
-                list.clear();
-                i++;
+            index ++;
+            if(index == m){
+                answer += score[i] * m;
+                index = 0;
             }
-            if (index == count ) break;
 
         }
 
