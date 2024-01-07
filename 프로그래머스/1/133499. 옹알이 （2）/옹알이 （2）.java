8d04554590c571e54caf3875510d6eebbca036e1
이상도 <108053426+SANGDOLEE@users.noreply.github.com>
2024-01-07 13:57:43 +0900

    [level 1] Title: 옹알이 (2), Time: 1.02 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String[] babbling) {
+           int answer = 0;
+
+        String[] temp = {"aya","ye","woo","ma"};
+        for(int i=0; i<babbling.length; i++){
+            String s = babbling[i];
+           
+            if(babbling[i].contains("ayaaya")||babbling[i].contains("yeye")||babbling[i].contains("woowoo")||babbling[i].contains("mama")){
+                continue;
+            }
+            
+            for(int j=0; j<temp.length; j++){
+                s=s.replace(temp[j]," ");
+            }
+            s=s.replace(" ","");
+            if(s.length()==0) answer++;
+        }
+        return answer;
+    }
+}
