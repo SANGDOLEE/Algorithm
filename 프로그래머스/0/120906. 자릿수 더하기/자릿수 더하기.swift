d07ce981c8ffd7b6004b211022afc4864a7df623
Mars <108053426+SANGDOLEE@users.noreply.github.com>
2024-06-23 16:18:40 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.swift
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.swift
@@ -1,11 +1,6 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var answer = 0 
     let text = String(n)
-    let array = text.compactMap { Int(String($0)) }
-    for i in array {
-        answer += i
-    }
-    return answer
+    return text.compactMap { Int(String($0)) }.reduce(0, +)
 }
