01185b22ab48f840b94f5cb9623f90c2676205be
Mars <108053426+SANGDOLEE@users.noreply.github.com>
2024-06-23 16:51:58 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.swift
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.swift
@@ -2,7 +2,6 @@ import Foundation
 
 func solution(_ n:Int, _ k:Int) -> Int {
     var food = n * 12000
-    var temp = (n/10) * 2000
-    var drink = k * 2000
-    return food + drink - temp
+    var drink = (k * 2000) - ( (n/10)*2000)
+    return food + drink
 }
