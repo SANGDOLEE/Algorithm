5ae11db2800d3ed31f337eb61fbd4027e0f61bce
Mars <108053426+SANGDOLEE@users.noreply.github.com>
2024-06-28 15:23:29 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ a:Int, _ b:Int) -> Int {
+    if a % 2 == 1 && b % 2 == 1 {
+        return a*a+b*b
+    } else if a % 2 == 0 && b % 2 == 1 || a % 2 == 1 && b % 2 == 0 {
+        return 2*(a+b)
+    } else {
+        return abs(a-b)
+    }
+    
+}
