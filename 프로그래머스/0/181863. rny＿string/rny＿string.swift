d0890baa8c7cd9844fdf771de625c7598bfce652
Mars <108053426+SANGDOLEE@users.noreply.github.com>
2024-07-16 19:27:46 +0900

    [level 0] Title: rny_string, Time: 0.47 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.swift
+++ 프로그래머스/0/181863. rny＿string/rny＿string.swift
@@ -1,16 +1,5 @@
 import Foundation
 
 func solution(_ rny_string:String) -> String {
-    var result = ""
-    result.reserveCapacity(rny_string.count)  // 미리 메모리 할당
-    
-    for char in rny_string {
-        if char == "m" {
-            result.append("rn")
-        } else {
-            result.append(char)
-        }
-    }
-    
-    return result
+   return rny_string.replacingOccurrences(of: "m", with: "rn")
 }
